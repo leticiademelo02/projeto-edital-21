8dba1503cf1399f9cbe0dca6560195fe32637e1b
Leticia Melo <leticia.melo@lais.huol.ufrn.br>
2022-07-30 13:05:21 -0300

    new file:   layouts/DashboardLayout/DashboardLayout.vue new file:   layouts/DashboardLayout/styles.scss modified:   package.json modified:   src/App.vue new file:   src/components/FooterBar.vue/FooterBar.vue new file:   src/components/FooterBar.vue/styles.scss deleted:    src/components/HelloWorld.vue new file:   src/components/NavBar.vue/NavBar.vue new file:   src/components/NavBar.vue/styles.scss modified:   src/router/index.js modified:   src/views/AboutView.vue modified:   src/views/HomeView.vue

---
+++ layouts/DashboardLayout/DashboardLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <NavBar />
+    <FooterBar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue/NavBar.vue";
+import FooterBar from "./components/FoooterBar.vue/FooterBar.vue";
+
+export default {
+  name: "DashboardLayout",
+  components: {
+    NavBar,
+    FooterBar,
+  },
+};
+</script>
+
+<style lang="scss" scoped src="./styles.scss" />
---
+++ src/components/NavBar.vue/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-link to="/">oi</router-link>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style lang="scss" scoped src="./styles.scss" />
